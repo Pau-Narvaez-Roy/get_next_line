fd6c999bf81a85c2b9bf576ef0cf4d786b25517e
Ptitchat <pau.roy.narvaez@gmail.com>
2026-06-23 12:19:43 +0200

    .h

---
+++ get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pnarvaez <pnarvaez@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/22 14:38:56 by pnarvaez          #+#    #+#             */
+/*   Updated: 2026/06/23 11:42:29 by pnarvaez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(char *src);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+
+# endif
+
+#endif
